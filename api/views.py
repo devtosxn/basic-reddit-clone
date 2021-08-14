37b8d8d552fa25c26a19b24f6579a76214b9d063
Tosin Ayodele <devtosxn@gmail.com>
2021-08-14 20:27:27 +0100

    add token authentication and modify list-create view

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
-from rest_framework import generics, status, response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import generics, status, response, permissions
 
 from .models import Post, Vote
 from .serializers import PostSerializer, VoteSerializer
@@ -9,7 +10,21 @@ from helpers.response import Response
 
 class PostView(generics.ListCreateAPIView):
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    authentication_classes = (TokenAuthentication,)
     queryset = Post.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = PostSerializer(queryset, many=True)
+        return Response({'posts': serializer.data, 'total': queryset.count()}, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(errors=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
