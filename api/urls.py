68aad7b048c529ea9ed18269f9d466d4066b861f
Tosin Ayodele <devtosxn@gmail.com>
2021-08-14 23:05:24 +0100

    add endpoint to retrieve, update and delete post instance

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from .views import PostView
+from .views import PostView, PostDetailView
 
 
 urlpatterns = [
     path('posts/', PostView.as_view(), name='posts'),
+    path('posts/<uuid:id>/', PostDetailView.as_view(), name='post-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -28,3 +28,38 @@ class PostView(generics.ListCreateAPIView):
             self.perform_create(serializer)
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(errors=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    authentication_classes = (TokenAuthentication,)
+    queryset = Post.objects.all()
+    lookup_url_kwarg = 'id'
+    lookup_field = 'id'
+
+    def get(self, request, id):
+        try:
+            post = Post.objects.get(id=id)
+            serializer = PostSerializer(post)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        except Post.DoesNotExist:
+            return Response(errors={'detail': 'post does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = PostSerializer(
+            instance, data=request.data, partial=partial)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(errors=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(data={'data': {}}, status=status.HTTP_204_NO_CONTENT)
