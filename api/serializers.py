9d311b0fd8403f6d2e52211b4ead9b1119630ed7
Tosin Ayodele <devtosxn@gmail.com>
2021-08-16 22:07:20 +0100

    add number of votes to for post instance to serializer

--- api/serializers.py
+++ api/serializers.py
@@ -4,11 +4,16 @@ from .models import Post, Vote
 
 
 class PostSerializer(serializers.ModelSerializer):
+    votes = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
         fields = ('id', 'title', 'content', 'created_at', 'updated_at',
-                  'author')
+                  'author', 'votes')
         extra_kwargs = {'author': {'read_only': True}}
+
+    def get_votes(self, obj):
+        return Vote.objects.filter(post=obj).count()
 
 
 class VoteSerializer(serializers.ModelSerializer):
